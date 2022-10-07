e2afde8a2a48dc105198518d09cfcc0e20f98f86
Liang.Mu <357737784@qq.com>
2022-10-07 15:16:27 +0800

    init project

---
+++ src/main/kotlin/com/example/kotlinwithaws/KotlinWithAwsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinwithaws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWithAwsApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWithAwsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinwithaws/KotlinWithAwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinwithaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWithAwsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
